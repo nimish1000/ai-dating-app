9bf7bd5928023ce40655f69894fa9d6c69ebdfa7
Nimish Sabnani <nimishsabnani@gmail.com>
2026-06-13 23:43:46 +0530

    Update swipe controller changes

--- server/src/controllers/swipe.controller.js
+++ server/src/controllers/swipe.controller.js
@@ -37,11 +37,8 @@ const normalizePhotoUrl = (req, url) => {
 // We need a raw pool client for transactions
 // Import the pool from database.js
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     max: 10,
 });
 
